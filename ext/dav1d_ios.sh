e070ea00d520a198ed557ea587095956b1175f5d
kiddkaffeine <miles@Ms-MacBook-Pro.local>
2025-12-30 22:46:34 -0500

    _

--- ext/dav1d_ios.sh
+++ ext/dav1d_ios.sh
@@ -13,8 +13,10 @@ cat ios_template.txt | sed "s|%SDKROOT%|${SDKROOT}|g" | sed "s|%DEVROOT%|${DEVRO
 git clone -b 1.5.1 --depth 1 https://code.videolan.org/videolan/dav1d.git
 
 cd dav1d
+mkdir -p build
+cd build
 
-MACOSX_DEPLOYMENT_TARGET=11.0 meson setup --cross-file=../ext/ios.txt --default-library=static --buildtype release .
+MACOSX_DEPLOYMENT_TARGET=11.0 meson setup --cross-file=../../ext/ios.txt --default-library=static --buildtype release ..
 MACOSX_DEPLOYMENT_TARGET=11.0 ninja
 
 find .
