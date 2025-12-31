4ff537b7e88fa885fdef0e485f15b4de7b441412
kiddkaffeine <miles@Ms-MacBook-Pro.local>
2025-12-30 22:45:12 -0500

    _

--- ext/dav1d_tvos.sh
+++ ext/dav1d_tvos.sh
@@ -14,7 +14,7 @@ git clone -b 1.5.1 --depth 1 https://code.videolan.org/videolan/dav1d.git
 
 cd dav1d
 
-MACOSX_DEPLOYMENT_TARGET=11.0 meson setup --cross-file=../ext/tvos.txt --default-library=static --buildtype release ..
+MACOSX_DEPLOYMENT_TARGET=11.0 meson setup --cross-file=../ext/tvos.txt --default-library=static --buildtype release .
 MACOSX_DEPLOYMENT_TARGET=11.0 ninja
 
 find .
